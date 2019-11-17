6f3988db0f42b3e39b0430c7d791d737858e8f45
yiratpeleg <yiratpeleg8@gmail.com>
2019-11-17 22:30:44 +0200

    update

--- main.c
+++ main.c
@@ -10,12 +10,11 @@ int main()
     }
     while (scanf("%lf" ,&num)!=1);
     double ans1 = add(Exp((int)num),sub(Pow(num,3),2));
-    double ans2 = add(mul(3,num),mul(2,Pow(num,2)));
-    double ans3 = div(mul(4,Pow(num,3)),sub(5,mul(2,num)));
+    double ans2 = add(mul(num,3),mul(Pow(num,2),2));
+    double ans3 = sub(div(mul(Pow(num,3),4),5),mul(num,2));
     printf("The value of f(x) = e^x+x^3-2 at the point %.4lf is: %.4lf\n",num,ans1);
     printf("The value of f(x) = 3x+2x^2 at the point %.4lf is: %.4lf\n",num,ans2 );
     printf("The value of f(x) = (4x^3)/5-2x at the point %.4lf is: %.4lf\n" ,num,ans3);
 
-//printf("%lf\n", Exp(1));
 
 }
